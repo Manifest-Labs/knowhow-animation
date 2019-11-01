8cf8b969ef8223059664cd02a6b3d399ca395702
Pedro Restrepo <pedro.j.restrepo@gmail.com>
2019-11-01 14:50:07 -0600

    Adding animations

---
+++ landing/index.js
@@ -0,0 +1,19 @@
+import attachDocs from './LP_attachDocs.json';
+import centralHub from './LP_centralHub.json';
+import editing from './LP_editing.json';
+import insights from './LP_insights.json';
+import mellenny from './millenny.json';
+import simplifyWork from './simplifyWork.json';
+import supervisorsSupport from './supervisorsSupport.json';
+import desktopMobile from './desktop-and-mobile.json';
+
+export default {
+  attachDocs,
+  centralHub,
+  editing,
+  insights,
+  mellenny,
+  simplifyWork,
+  supervisorsSupport,
+  desktopMobile,
+};
---
+++ site/index.js
@@ -0,0 +1,31 @@
+import logoLoader from './logoLoader.json';
+import loader from './loader.json';
+import ellipsisToX from './ellipsisToX.json';
+import mountaineering from './mountaineering.json';
+import confused from './confused.json';
+import gears from './gears.json';
+import office from './office.json';
+import computerSearch from './computer_search.json';
+import highfive from './highfive.json';
+import magnifyingGlassPaper from './magnifying-glass-paper.json';
+import sittingpadlock from './sittingpadlock.json';
+import carrotOpen from './carrotOpen.json';
+import carrotClose from './carrotClose.json';
+import browseLoader from './recipePage_Browse_loader.json';
+
+export default {
+  loader,
+  logoLoader,
+  ellipsisToX,
+  mountaineering,
+  confused,
+  gears,
+  office,
+  computerSearch,
+  highfive,
+  magnifyingGlassPaper,
+  sittingpadlock,
+  carrotOpen,
+  carrotClose,
+  browseLoader,
+};
