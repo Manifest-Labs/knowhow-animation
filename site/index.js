fb0b4a1c24273c0363d1a4ec823fe6ce43a91dfa
Cody Giles <hicodygiles@gmail.com>
2019-11-04 13:29:15 -0700

    Added loader names into the file.

--- site/index.js
+++ site/index.js
@@ -12,6 +12,9 @@ import sittingpadlock from './sittingpadlock.json';
 import carrotOpen from './carrotOpen.json';
 import carrotClose from './carrotClose.json';
 import browseLoader from './recipePage_Browse_loader.json';
+import half-sec_loader from './half-sec_loader.json';
+import kh_logo_loader_DARK from './kh_logo_loader_DARK.json';
+import kh_logo_loader_WHITE from './kh_logo_loader_WHITE.json';
 // ADD import (like above) and export (like below) everytime a new animation is created
 export default {
   loader,
@@ -28,4 +31,7 @@ export default {
   carrotOpen,
   carrotClose,
   browseLoader,
+  kh_logo_loader_WHITE,
+  kh_logo_loader_DARK,
+  half-sec_loader,
 };
