69ce7f6a7be69d7f950709af365fae2faca47ded
Pedro Restrepo <pedro.j.restrepo@gmail.com>
2022-02-02 10:03:13 -0700

    know-1247: Gradient animation + mobile folder structure

---
+++ mobile/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _gradient = _interopRequireDefault(require("./gradient.json"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// ADD import (like above) and export (like below) everytime a new animation is created
+var _default = {
+  gradient: _gradient.default,
+};
+exports.default = _default;
