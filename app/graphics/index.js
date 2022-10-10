3ddc7a8ca9bc222ebfb5481ea5b1fe6c2d21eb93
Eleazar Maestre <eleazarenrique23@gmail.com>
2022-10-10 11:11:45 -0400

    know-1732: Add lottie file for onboarding flow

--- app/graphics/index.js
+++ app/graphics/index.js
@@ -11,10 +11,15 @@ var _computerSearch = _interopRequireDefault(require("./computer_search.json"));
 var _confused = _interopRequireDefault(require("./confused.json"));
 var _gears = _interopRequireDefault(require("./gears.json"));
 var _highfive = _interopRequireDefault(require("./highfive.json"));
-var _magnifyingGlass = _interopRequireDefault(require("./magnifying-glass-paper.json"));
+var _magnifyingGlass = _interopRequireDefault(
+  require("./magnifying-glass-paper.json")
+);
 var _mountaineering = _interopRequireDefault(require("./mountaineering.json"));
 var _office = _interopRequireDefault(require("./office.json"));
 var _sittingPadlock = _interopRequireDefault(require("./sittingpadlock.json"));
+var _constructionPeople = _interopRequireDefault(
+  require("./construction-people.json")
+);
 
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
@@ -30,5 +35,6 @@ var _default = {
   mountaineering: _mountaineering.default,
   office: _office.default,
   sittingPadlock: _sittingPadlock.default,
+  constructionPeople: _constructionPeople.default,
 };
 exports.default = _default;
