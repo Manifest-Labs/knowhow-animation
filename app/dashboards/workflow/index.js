b004a83c848781fedbc2a7e43a5bbd85e5548c71
Pedro Restrepo <pedro.j.restrepo@gmail.com>
2024-01-02 18:48:35 +0100

    knowhow-448: Adding also the full loader for workflows

--- app/dashboards/workflow/index.js
+++ app/dashboards/workflow/index.js
@@ -1,8 +1,10 @@
+import workflowFullDesktop from "./workflow_full_desktop.json";
 import workflowHeaderDesktop from "./workflow_header_desktop.json";
 import workflowTimelineDesktop from "./workflow_timeline_desktop.json";
 import workflowTopInformationDesktop from "./workflow_top_information_desktop.json";
 
 export {
+  workflowFullDesktop,
   workflowHeaderDesktop,
   workflowTimelineDesktop,
   workflowTopInformationDesktop,
