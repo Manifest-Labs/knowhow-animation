70e0b50309e9e40426205ee37493d7acb3c3808c
Eleazar Maestre <eleazarenrique23@gmail.com>
2024-02-05 14:58:05 -0700

    knowhow-318: Add project summary creation lottie

--- app/dashboards/skillBuilder/index.js
+++ app/dashboards/skillBuilder/index.js
@@ -1,3 +1,4 @@
+import projectSummaryCreation from "./project_summary_creation.json";
 import questionEdit from "./question_edit.json";
 
-export { questionEdit };
+export { projectSummaryCreation, questionEdit };
