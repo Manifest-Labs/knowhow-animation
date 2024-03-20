fcac5d9de4f61648898eb608dbf0b4b12701fbda
PedroRestrepo <pedro.j.restrepo@gmail.com>
2024-03-20 19:26:53 +0100

    knowhow-623: Adding the Doc Talk animations

---
+++ app/dashboards/docTalk/index.js
@@ -0,0 +1,6 @@
+import chatLoading from "./chat_loading.json";
+import chatHeader from "./chat_header.json";
+import chatTyping from "./chat_typing.json";
+import documentLoading from "./document_loading.json";
+
+export { chatLoading, chatHeader, chatTyping, documentLoading };
