30666cd95f2830c5285104b8ddb91eac6e6b7604
Andres Ramirez Restrepo <andres@tryknowhow.com>
2024-05-15 09:59:43 -0600

    knowhow-586: Adding animations for template library feature page loader.

---
+++ app/dashboards/featureTemplates/index.js
@@ -0,0 +1,5 @@
+import templateLibraryFeatureHeader from "./template-library-header.json";
+import templateLibraryFeatureTitle from "./template-library-title.json";
+import templateLibraryFeatureImage from "./template-library-image.json";
+
+export { templateLibraryFeatureHeader, templateLibraryFeatureTitle, templateLibraryFeatureImage};
