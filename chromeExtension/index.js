abb9f3f1f8baa2ee049a81531e2f790a01f530a5
Eleazar Maestre <eleazarenrique23@gmail.com>
2024-09-04 16:10:59 -0600

    knowhow-904: Update lottie imports

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,26 +1,10 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true,
-});
-
-exports.default = void 0;
-
-// Chrome Extension
-var _processDetailLoading = _interopRequireDefault(require("./process_detail_loading.json"));
-var _processRecommended = _interopRequireDefault(require("./process_recommended.json"));
-var _transcriptionLoading = _interopRequireDefault(require("./transcription_loading.json"));
+import processDetailLoading from "./process_detail_loading.json";
+import processRecommended from "./process_recommended.json";
+import transcriptionLoading from "./transcription_loading.json";
 
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
-// ADD import (like above) and export (like below) everytime a new animation is created
-var _default = {
-  processDetailLoading: _processDetailLoading.default,
-  processRecommended: _processRecommended.default,
-  transcriptionLoading: _transcriptionLoading.default,
+export {
+  processDetailLoading,
+  processRecommended,
+  transcriptionLoading,
 };
-
-exports.default = _default;
