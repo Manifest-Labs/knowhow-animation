e92cbf3c6446c6227cfaa0525c763adbe2537b7e
Andres Ramirez Restrepo <andres@tryknowhow.com>
2025-04-28 14:11:09 -0600

    knowhow-1112: Adding tablet explore more lottie.

--- app/dashboards/explore/index.js
+++ app/dashboards/explore/index.js
@@ -4,6 +4,7 @@ import exploreMore from "./explore_more.json";
 import exploreSkillBuilderReview from "./explore_skill_builder_review.json";
 import exploreTags from "./explore_tags.json";
 import exploreTopProcesses from "./explore_top_processes.json";
+import exploreTopProcessesMobile from "./explore_top_processes_mobile.json";
 
 export {
     exploreBookmark,
@@ -12,4 +13,5 @@ export {
     exploreSkillBuilderReview,
     exploreTags,
     exploreTopProcesses,
-}
+    exploreTopProcessesMobile,
+};
