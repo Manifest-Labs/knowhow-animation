811308f140a005c6cc4b7835d83c671584c1872f
maxvilcapomag <max.vilcapomag@gmail.com>
2026-07-24 17:03:50 -0600

    knowhow-2348: Add Skill Builder Modal Loading Lottie

--- app/dashboards/skillBuilder/index.js
+++ app/dashboards/skillBuilder/index.js
@@ -1,4 +1,5 @@
 import projectSummaryCreation from "./project_summary_creation.json";
 import questionEdit from "./question_edit.json";
+import skillBuilderLoadingModal from "./skill_builder_loading.json";
 
-export { projectSummaryCreation, questionEdit };
+export { projectSummaryCreation, questionEdit, skillBuilderLoadingModal};
